40c62c9fa2846bac764268846bad42594f95f287
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2023-11-29 21:52:32 +0000

    updated theme

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import flet as ft
 from flet import RoundedRectangleBorder
-import openai as ai
+from openai import OpenAI
 from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
-ai.api_key = getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=getenv("OPENAI_API_KEY"))
 
 
 def main(page: ft.Page):
@@ -19,14 +19,14 @@ def main(page: ft.Page):
             tbQuestion.value = ""
             lvAnswer.controls.append(ft.Text(f"You: {question}"))
             page.update()
-            completion = ai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
+            completion = client.chat.completions.create(
                 messages=[{"role": "user", "content": question}],
+                model="gpt-3.5-turbo",
             )
             lvAnswer.controls.append(
                 ft.Text(
                     f"ChatGPT: {completion.choices[0].message.content}",
-                    color=ft.colors.BLUE_ACCENT,
+                    color=ft.colors.TERTIARY,
                 )
             )
 
@@ -35,42 +35,21 @@ def main(page: ft.Page):
             page.banner.open = True
             page.update()
 
-    page.title = "ChatGPT"
-    page.window_width = 645
-    page.window_height = 455
+    page.window_width = 600
+    page.window_height = 400
     page.window_resizable = False
     page.window_full_screen = False
     page.scroll = ft.ScrollMode(value="auto")
-    page.theme_mode = "dark"
     page.window_maximizable = False
-    page.window_title_bar_hidden = True
-    page.window_title_bar_buttons_hidden = True
-
-    windowDragArea = ft.WindowDragArea(
-        ft.Container(
-            ft.Text("ChatGPT", size=20), bgcolor=ft.colors.BACKGROUND, padding=10
-        ),
-        expand=True,
-        maximizable=False,
-    )
-
-    btnClose = ft.IconButton(
-        ft.icons.CLOSE,
-        style=ft.ButtonStyle(
-            color={
-                ft.MaterialState.DEFAULT: ft.colors.WHITE60,
-                ft.MaterialState.HOVERED: ft.colors.RED_ACCENT_200,
-            },
-            shape={ft.MaterialState.DEFAULT: RoundedRectangleBorder(radius=10)},
-        ),
-        on_click=lambda _: page.window_close(),
-    )
+    page.theme = ft.Theme(use_material3=True, color_scheme_seed="cyan")
 
     page.banner = ft.Banner(
-        bgcolor=ft.colors.AMBER_ACCENT_700,
-        leading=ft.Icon(ft.icons.WARNING_AMBER_ROUNDED, color=ft.colors.BLACK, size=40),
+        bgcolor=ft.colors.ERROR_CONTAINER,
+        leading=ft.Icon(
+            ft.icons.WARNING_AMBER_ROUNDED, color=ft.colors.ON_ERROR_CONTAINER, size=40
+        ),
         content=ft.Text(
-            color=ft.colors.BLACK,
+            color=ft.colors.ON_ERROR_CONTAINER,
             value="ChatGPT was unable to respond to that message. Please try again.",
         ),
         actions=[
@@ -79,10 +58,7 @@ def main(page: ft.Page):
                 on_click=btnClick,
                 style=ft.ButtonStyle(
                     color={
-                        ft.MaterialState.DEFAULT: ft.colors.BLACK,
-                    },
-                    bgcolor={
-                        ft.MaterialState.HOVERED: ft.colors.AMBER_ACCENT_400,
+                        ft.MaterialState.DEFAULT: ft.colors.ON_ERROR_CONTAINER,
                     },
                     shape={ft.MaterialState.DEFAULT: RoundedRectangleBorder(radius=10)},
                 ),
@@ -92,10 +68,10 @@ def main(page: ft.Page):
                 on_click=closeBanner,
                 style=ft.ButtonStyle(
                     color={
-                        ft.MaterialState.DEFAULT: ft.colors.BLACK,
+                        ft.MaterialState.DEFAULT: ft.colors.ON_ERROR_CONTAINER,
                     },
                     bgcolor={
-                        ft.MaterialState.HOVERED: ft.colors.AMBER_ACCENT_400,
+                        ft.MaterialState.HOVERED: ft.colors.ERROR_CONTAINER,
                     },
                     shape={ft.MaterialState.DEFAULT: RoundedRectangleBorder(radius=10)},
                 ),
@@ -104,11 +80,11 @@ def main(page: ft.Page):
     )
 
     lvAnswer = ft.ListView(
-        expand=1,
+        expand=True,
         spacing=10,
         padding=20,
         auto_scroll=True,
-        height=290,
+        height=page.window_height - 150,
     )
 
     tbQuestion = ft.TextField(
@@ -118,28 +94,26 @@ def main(page: ft.Page):
         shift_enter=True,
         border_radius=10,
         border_color=ft.colors.BACKGROUND,
-        text_style=ft.TextStyle(color=ft.colors.WHITE70),
-        label_style=ft.TextStyle(color=ft.colors.WHITE70),
+        text_style=ft.TextStyle(color=ft.colors.ON_SURFACE_VARIANT),
+        label_style=ft.TextStyle(color=ft.colors.ON_SURFACE_VARIANT),
         border_width=2,
         focused_border_width=4,
         expand=True,
         on_submit=btnClick,
         autofocus=True,
-        focused_border_color=ft.colors.BLUE_ACCENT,
+        focused_border_color=ft.colors.ON_SURFACE_VARIANT,
         capitalization=ft.TextCapitalization.SENTENCES,
         focused_bgcolor=ft.colors.BACKGROUND,
-        bgcolor=ft.colors.BLACK26,
-        cursor_color=ft.colors.BLUE_ACCENT,
-        cursor_width=3,
+        bgcolor=ft.colors.SURFACE_VARIANT,
+        cursor_color=ft.colors.ON_SURFACE_VARIANT,
+        cursor_width=2,
     )
 
     page.add(
-        ft.Row(controls=[windowDragArea, btnClose]),
         ft.Row(controls=[lvAnswer]),
         ft.Row(controls=[tbQuestion], alignment="bottom"),
-    ),
-
-    pass
+    )
 
 
-ft.app(target=main)
+if __name__ == "__main__":
+    ft.app(target=main)
